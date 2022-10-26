b23bb5851ac970fdf6488760c367393a7d049c6d
tawhidhaider <tawhidalam69@gmail.com>
2022-10-26 19:34:57 +0600

    courses

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const Port = process.env.Port || 5000;
 
 app.use(cors());
 
+const categories = require('./Data/Categories.json');
+app.get('/course-categories',(req,res)=>{
+    res.send(categories);
+})
+const courses = require('./Data/Courses.json');
+
+
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedcategory = courses.filter(course => course.category_id ===id);
+    res.send(selectedcategory);
+
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses);
+})
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedcourses = courses.find(course=>course._id ===id);
+    res.send(selectedcourses);
+})
+
 app.listen(Port,()=>{
     console.log("serverr on",Port);
 });
